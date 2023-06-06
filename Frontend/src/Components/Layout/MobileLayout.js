d7e9d31d6cba0d221b1cddcd62159ce3185c2930
Dhruvil <dhruvil.mehta@inferenz.ai>
2023-06-06 13:45:27 +0530

    [UPD] Minor Change

--- Frontend/src/Components/Layout/MobileLayout.js
+++ Frontend/src/Components/Layout/MobileLayout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Overview from '../Overview'
 import SkinAge from '../SkinAge'
 import AverageSkinScore from '../AverageSkinScore'
@@ -6,31 +6,44 @@ import SkinAnalysis from '../SkinAnalysis'
 import SkinConcern from '../SkinConcern'
 import Summary from '../Summary'
 import ResultView from '../ResultView/inedx'
+import { useLocation, useNavigate, useNavigationType } from 'react-router-dom'
 
 export default function MobileLayout() {
+  const navType = useNavigationType();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (navType === 'POP') {
+            navigate('/');
+        }
+    }, [])
+
+    let { state } = useLocation();
+    if (state) {
+        state = JSON.parse(state);
+    }
   return (
     <>
       <div className="container mt-4 bor">
         <div className="">
-          <ResultView />
+          <ResultView resultedImage = {state._response}/>
         </div>
         <div className="mt-2 bor">
-          <Overview/>
+          <Overview analyzedData={ state._response}/>
         </div>
         <div className="mt-2 bor">
-          <SkinAge/>
+          <SkinAge resultedImage = {state._response}/>
         </div>
         <div className="mt-2 bor">
-          <SkinAnalysis/>
+          <SkinAnalysis resultedImage = {state._response}/>
         </div>
         <div className="mt-2 bor">
-          <SkinConcern/>
+          <SkinConcern resultedImage = {state._response}/>
         </div>
         <div className="mt-2 bor">
-          <Summary/>
+          <Summary resultedImage = {state._response}/>
         </div>
         <div className="mt-2 bor">
-          <AverageSkinScore/>
+          <AverageSkinScore resultedImage = {state._response}/>
         </div>
       </div>
     </>
